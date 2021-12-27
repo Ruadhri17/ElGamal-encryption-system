9517ed46bbc60eedb2609bfa16c49d011838f7b5
Ruadhri17 <K.R.Piotrowski17@gmail.com>
2021-12-27 19:39:55 +0100

    add system that choose generator

--- cipher.py
+++ cipher.py
@@ -1,6 +1,5 @@
-import sys
-from random import randint
-from math import pow, sqrt
+from random import randint, choice
+from math import sqrt, gcd
 
 def isPrime(n):
     if n == 2:
@@ -17,31 +16,39 @@ def isPrime(n):
     return True
 
 def cyclicGroupDescription(p):
-    #Calculate order
+    # Order of prime cyclic group is p - 1 as it has p - 1 elements in group
     q = p - 1
-    #Get generator
-    for g in range(1, q): 
-        if (pow(g, q) % p) == 1:
-            break;          
-    return (p, q, g) 
+    #Get generator base
+    g = []
+    base = findGeneratorBase(q)
+    # find all coprimes to p - 1 (when divisor of p - 1 and number is only 1 then number is coprime)
+    for coprime in range(1, q):
+        if gcd(coprime, q) == 1:
+            g.append(pow(base, coprime, p)) # every number base^coprime % prime is generator 
+    # Return: group, order, generator
+    return (p, q, choice(g)) 
 
-def generateKey(p, q, g):
+def findGeneratorBase(elements):
+    # Start from 2 as 1 cannot be generator base
+    base = 2
+    testBase = []
+    # base^element % prime must give unique numbers for all elements
+    while True:
+        for element in range (1, elements):
+            testBase.append(pow(base, element, elements + 1))
+        # if numbers are unique then return base of generators else check another one
+        if len(testBase) == len(set(testBase)):
+            return base 
+        else:
+            base += 1
+            testBase.clear()
+def generateKeys(p, q, g):
     x = randint(1, q - 1) # Private Key
     h = pow(g, x)
     publicKey = (p, q, g, h)
     privateKey = x
     return (publicKey, privateKey)
 
-def encryption(message, key):
-    m = map(message)
-    y = randint(1, key[1])
-    s = pow(key[3], y)
-    c1 = pow(key[2], y)
-    c2 = m * s
-    return
-def decryption(message, key):
-    return
-    
 def main():
     try:
         value = int(input("Choose cyclic group size (enter prime number): "))
@@ -53,10 +60,10 @@ def main():
         return
     else:
         (group , order , generator) = cyclicGroupDescription(value)
-        (publicKey, privateKey) = generateKey(group, order, generator)
-        encryption(message, publicKey)
-        decryption(privateKey)
-        return
+        (publicKey, privateKey) = generateKeys(group, order, generator)
+        #encryption(message, publicKey)
+        #decryption(privateKey)
+        #return
 
 if __name__ == "__main__":
     main()
