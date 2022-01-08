20fa6b7c601039a9d8cb9ddcd6a79334fb016342
Ruadhri17 <K.R.Piotrowski17@gmail.com>
2022-01-08 17:45:22 +0100

    Add algorithm for finding large prime numbers

--- cipher.py
+++ cipher.py
@@ -1,4 +1,4 @@
-from random import randint, choice
+from random import randint, choice, getrandbits
 from math import sqrt, gcd
 
 def isPrime(n):
@@ -15,13 +15,63 @@ def isPrime(n):
             return False
     return True
 
+def millerRabinTestMultiple(nr, nrOfTimes):
+    while(nrOfTimes > 0):
+        if millerRabinTest(nr) == False:
+            return False
+        nrOfTimes = nrOfTimes - 1
+    return True
+
+def millerRabinTest(nr):
+    d = nr - 1
+    while (d % 2 == 0):
+        d //= 2
+
+    a = 2 + randint(1, nr - 4)
+
+    x = pow(a, d, nr)
+
+    if (x == 1 or x == nr - 1):
+        return True
+
+    while (d != nr - 1):
+        x = (x * x) % nr
+        d *= 2
+
+        if (x == 1):
+            return False
+        if (x == nr - 1):
+            return True
+
+    return False
+
+def fermatPrimalityTest(nr, nrOfTimes):
+    for i in range(nrOfTimes):
+    
+        a = randint(2, nr - 2)
+
+        if pow(a, nr - 1, nr) != 1:
+            return False
+    return True
+
+def choosePrime(numberOfBits):
+    while True:
+        primeCandidate = getrandbits(numberOfBits)
+        if primeCandidate % 2 == 0:
+            continue
+        if not fermatPrimalityTest(primeCandidate, 1000):
+            continue
+        if not millerRabinTestMultiple(primeCandidate, 1000):
+            continue
+        return primeCandidate
+    
 def cyclicGroupDescription(p):
     # Order of prime cyclic group is p - 1 as it has p - 1 elements in group
     q = p - 1
-    #Get generator base
+    # Get generator base
     g = []
     base = findGeneratorBase(q)
-    # find all coprimes to p - 1 (when divisor of p - 1 and number is only 1 then number is coprime)
+    # Find all coprimes to p - 1 (when divisor of p - 1 and number is only 1 then number is coprime)
     for coprime in range(1, q):
         if gcd(coprime, q) == 1:
             g.append(pow(base, coprime, p)) # every number base^coprime % prime is generator 
@@ -44,28 +94,30 @@ def findGeneratorBase(elements):
             testBase.clear()
 
 def generateKeys(p, q, g):
-    x = randint(1, q - 1) # Private Key
-    h = pow(g, x)
+    x = randint(2, q - 1) # Private Key
+    h = pow(g, x, p)
     publicKey = (p, q, g, h)
     privateKey = x
     return (publicKey, privateKey)
 
-def encryption(message, publicKey):
-    y = randint(1, publicKey[1] - 1) # 1 .. q - 1
-    s = pow(publicKey[3], y) # h^y
-    c1 = pow(publicKey[2], y) # g^y
-    c2 = message ^ s
+def encryption(message, publicKey, group):
+    y = randint(2, publicKey[1] - 1) # 2 .. q - 1
+    s = pow(publicKey[3], y, publicKey[0]) # h^y
+    c1 = pow(publicKey[2], y, publicKey[0]) # g^y
+    c2 = (message * s ) % publicKey[0]
     return (c1, c2)
 
 def decryption(c1, c2, privateKey, group):
-    s = pow(c1, privateKey)
-    # compute inverse
-    inverse = pow(s, group - 2, group)
-    #compute m
-    m = c2 ^ inverse
+    s = pow(c1, group - 1 - privateKey, group)
+    # Compute inverse
+    # inverse = pow(s, group - 2, group)
+    # Compute m
+    m = (c2 * s) % group
     return m
 
 def main():
+    prime = choosePrime(1024)
+    print(prime)
     try:
         value = int(input("Choose cyclic group size (enter prime number): "))
     except ValueError:
@@ -75,9 +127,13 @@ def main():
         print("Input is not prime number!")
         return
     else:
-        (group , order , generator) = cyclicGroupDescription(value)
+        (group , order , generator) = cyclicGroupDescription(prime)
+        print("Chosen generator: ", generator)
+
         (publicKey, privateKey) = generateKeys(group, order, generator)
-        (c1, c2) = encryption(43, publicKey)
+
+        (c1, c2) = encryption(3000, publicKey, group)
+
         message = decryption(c1, c2, privateKey, group)
         print(message)
 
